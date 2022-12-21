c4c511be6f3a18eb6f5021e008eea911486136f2
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-12-21 14:04:09 +0900

    add gpt2 tokenizer

--- gpt3_generation_kor.py
+++ gpt3_generation_kor.py
@@ -1,36 +1,42 @@
 import transformers
-from transformers import BertTokenizerFast, TFGPT2LMHeadModel, GPT2LMHeadModel
+from transformers import BertTokenizerFast, TFGPT2LMHeadModel, GPT2LMHeadModel, GPT2Tokenizer
 transformers.logging.set_verbosity_error()
 
 class Inference():
     def __init__(self, model_name, tf_pt='tf'):
         self.tf_pt = tf_pt
 
-        self.tokenizer = BertTokenizerFast.from_pretrained(model_name)
+        # self.tokenizer = BertTokenizerFast.from_pretrained(model_name)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         if self.tf_pt == 'tf':
             self.model = TFGPT2LMHeadModel.from_pretrained(model_name, pad_token_id=self.tokenizer.eos_token_id)
         else:
             self.model = GPT2LMHeadModel.from_pretrained(model_name, pad_token_id=self.tokenizer.eos_token_id)
 
-    def generate(self, text, howmany=3):
+    def generate(self, text):
         input_ids = self.tokenizer.encode(text, return_tensors='tf' if self.tf_pt=='tf' else 'pt')
         input_ids = input_ids[:, 1:]  # remove cls token
 
         outputs = self.model.generate(
             input_ids,
-            min_length=30,
-            max_length=50,
-            do_sample=True,
-            top_k=10,
-            top_p=0.95,
-            no_repeat_ngram_size=2,
-            num_return_sequences=howmany
+            max_length=200,
+            num_beams = 5,
+            no_repeat_ngram_size = 2,
+            early_stopping = True,
+            min_length=100,
+            # do_sample=True,
+            # top_k=10,
+            # top_p=0.95,
+            # no_repeat_ngram_size=2,
+            # num_return_sequences=howmany
         )
 
-        for idx, generated in enumerate(
-                [self.tokenizer.decode(sentence, skip_special_tokens=True) for sentence in outputs]):
-            print('{0}: {1}'.format(idx, generated))
+        print("Output:\n" + 100 * "-")
+        print(self.tokenizer.decode(outputs[0], skip_special_tokens=True))
+        # for idx, generated in enumerate(
+        #         [self.tokenizer.decode(sentence, skip_special_tokens=True) for sentence in outputs]):
+        #     print('{0}: {1}'.format(idx, generated))
             
-infer = Inference(model_name="kykim/bert-kor-base")
-text = "조지아주 출신 작가"
+infer = Inference(model_name="gpt2")
+text = "The fortune of Lakpa for next year is"
 infer.generate(text = text)
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, BertTokenizerFast, BertModel
+from transformers import AutoTokenizer, BertTokenizerFast, BertModel, AutoModelForMaskedLM
 
 
 def training_tokenizer():
@@ -17,8 +17,15 @@ def korean_tokenizer():
     tokenizer_bert = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
     model_bert = BertModel.from_pretrained("kykim/bert-kor-base")
     print("ok done")
-    example = "조지아주 출신 작가 분류:소련-아프가니스탄 전쟁 관련자 분류:20세기 미국 사람 분류:21세기 미국 사람"
+    example = "정답은 테슬라일 수도, 아닐 수도 있는데요. ‘전기차’의 기준을 어디에 두느냐에 따라 다릅니다. \
+        ‘순수전기차(내연기관 아예 없음)’ 판매량으로는 테슬라가 단연 세계 1위 맞는데요"
     tokens = tokenizer_bert.tokenize(example)
     print(tokens)
 
+def automask_hangul():
+    tokenizer = AutoTokenizer.from_pretrained("kykim/bert-kor-base")
+    model = AutoModelForMaskedLM.from_pretrained("kykim/bert-kor-base")
+    
+    inputs = tokenizer("The capital of [MASK].", return_tensors="pt")
+    pass
 korean_tokenizer()
